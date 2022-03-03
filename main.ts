8af2278fd7adfc2468aa49b3fe1f2263f03b9dc1
Paul <pauljavaloyesdev@gmail.com>
2022-03-03 17:11:06 +0100

    Add worksheetsToJson util

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
 import xlsxlib from "xlsxlib";
 import type { XLSX } from "types";
-import { getWorkBook } from "utils";
+import { getWorkBook, getWorksheet, worksheetsToJson } from "utils";
 import { config } from "env";
 
 const xlsx = xlsxlib as XLSX;
 
 const workbook = await getWorkBook(config().XLSX_PATH);
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const json = await xlsx.utils.sheet_to_json(sheet);
 
+//Worksheet to JSON
+const sheet = getWorksheet(workbook, workbook.SheetNames[0]); //workbook.Sheets[workbook.SheetNames[0]];
+const json = xlsx.utils.sheet_to_json(sheet);
 console.log(json);
+
+//Worksheets to JSON
+const worksheetToJson = worksheetsToJson(workbook, workbook.SheetNames);
+console.log(worksheetToJson);
--- utils/xlsx.utils.ts
+++ utils/xlsx.utils.ts
@@ -1,8 +1,23 @@
 import xlsxlib from "xlsxlib";
-import type { WorkBook, XLSX } from "types";
+import type { WorkBook, WorkSheet, XLSX } from "types";
 const xlsx = xlsxlib as XLSX;
 
 export const getWorkBook = async (path: string): Promise<WorkBook> => {
   const rawText = await Deno.readFile(path);
   return await xlsx.read(rawText, { type: "buffer" });
+};
+
+export const getWorksheet = (
+  workbook: WorkBook,
+  sheetName: string,
+): WorkSheet => workbook.Sheets[sheetName];
+
+export const worksheetsToJson = (workbook: WorkBook, worksheets: string[]) => {
+  return worksheets.reduce((a, c) => {
+    const sheet = getWorksheet(workbook, c);
+    return {
+      ...a,
+      [c]: xlsx.utils.sheet_to_json(sheet),
+    };
+  }, {});
 };
