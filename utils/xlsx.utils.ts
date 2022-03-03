b1c6958458ebfa368a9948cc901935ffc7828b5f
Paul <pauljavaloyesdev@gmail.com>
2022-03-03 17:52:10 +0100

    exception handling in getWorkBook

--- utils/xlsx.utils.ts
+++ utils/xlsx.utils.ts
@@ -2,9 +2,13 @@ import xlsxlib from "xlsxlib";
 import type { WorkBook, WorkSheet, XLSX } from "types";
 const xlsx = xlsxlib as XLSX;
 
-export const getWorkBook = async (path: string): Promise<WorkBook> => {
-  const rawText = await Deno.readFile(path);
-  return await xlsx.read(rawText, { type: "buffer" });
+export const getWorkBook = (path: string): WorkBook => {
+  try {
+    const text = Deno.readFileSync(path)
+    return xlsx.read(text, { type: 'buffer' });
+  } catch (err) {
+    throw new Error(err)
+  }  
 };
 
 export const getWorksheet = (workbook: WorkBook, sheetName: string): WorkSheet => workbook.Sheets[sheetName];
